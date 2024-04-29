a51e0efda4e862673261b40e5dbcedbdafbb0b29
Andreas Kollegger <akollegger@gmail.com>
2024-04-29 08:41:32 +0100

    renamed test

---
+++ packages/gram/src/lib/parser/parser-stats.spec.ts
@@ -0,0 +1,41 @@
+import { GramSyntaxNode, parse, stats, GramSemanticStats } from "./parser";
+
+describe("gram parsed CST", () => {
+  it("stats should count nodes", () => {
+    const parseTree = parse("(hello)");
+    const gramStats = stats(parseTree.rootNode as GramSyntaxNode);
+    expect(gramStats["node"]).toBe(1);
+  });
+  it("stats should count nodes", () => {
+    const parseTree = parse("(hello),(world)");
+    const gramStats = stats(parseTree.rootNode as GramSyntaxNode);
+    expect(gramStats["node"]).toBe(2);
+  });
+
+  it("stats should count relationships", () => {
+    const parseTree = parse("(hello)-->(world)");
+    const gramStats = stats(parseTree.rootNode as GramSyntaxNode);
+    expect(gramStats["single_right"]).toBe(1);
+    expect(gramStats["node"]).toBe(2);
+  });
+
+  it("stats should count relationships", () => {
+    const parseTree = parse("(a)--(b)==(c)~~(d)-->(e)<--(f)==>(g)<==(h)~~>(i)<~~(j)");
+    const gramStats = stats(parseTree.rootNode as GramSyntaxNode);
+    expect(gramStats).toEqual({
+      node: 10,
+      single_undirected: 1,
+      single_bidirectional: 0,
+      single_right: 1,
+      single_left: 1,
+      double_undirected: 1,
+      double_bidirectional: 0,
+      double_right: 1,
+      double_left: 1,
+      squiggle_undirected: 1,
+      squiggle_bidirectional: 0,
+      squiggle_right: 1,
+      squiggle_left: 1
+    } as GramSemanticStats);
+  });
+})
